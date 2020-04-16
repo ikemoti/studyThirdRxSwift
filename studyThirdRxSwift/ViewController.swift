af6b02a37a73dbce36c2c5e98579b90943ba51f2
ikemoti <ikepon413@icloud.com>
2020-04-16 23:16:55 +0900

    Initial Commit

---
+++ studyThirdRxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  studyThirdRxSwift
+//
+//  Created by USER on 2020/04/16.
+//  Copyright © 2020 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ studyThirdRxSwiftTests/studyThirdRxSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  studyThirdRxSwiftTests.swift
+//  studyThirdRxSwiftTests
+//
+//  Created by USER on 2020/04/16.
+//  Copyright © 2020 USER. All rights reserved.
+//
+
+import XCTest
+@testable import studyThirdRxSwift
+
+class studyThirdRxSwiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
